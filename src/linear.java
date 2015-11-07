9928c2dab84f4354cc065ea78fa9baf0b5defa7b
eholzhauser <erwinholzhauser@gmail.com>
2015-11-07 15:44:06 -0500

    Added fixes to Gotoh's algorithm

--- src/linear.java
+++ src/linear.java
@@ -33,23 +33,20 @@ public class linear {
 	static double cc[], dd[], rr[], ss[];
 
 	public static void main(String args[]) {
-
-		// Example 1 from Myers and Miller (1987).
-		// A = "agtac";
-		// B = "aag";
-		// int m = A.length(), n = B.length();
-		// cc = new double[n + 1];
-		// dd = new double[n + 1];
-		// computeCost(m, n, cc, dd, g);
-		// for (double d : cc)
-		// System.out.print(d + " ");
-		// System.out.print("\n");
-		// for (double d : dd)
-		// System.out.print(d + " ");
-		// System.out.print("\n");
-
-		// new linear("agtac", "aag");
-		// diff(0, 0, A.length(), B.length());
+		new linear("agtac", "aag");
+		
+		 // Example 1 from Myers and Miller (1987).
+//		 computeCost(0, 0, A.length(), B.length(), cc, dd, g, true);
+//		 for (double d : cc)
+//		 System.out.print(d + " ");
+//		 System.out.print("\n");
+//		 for (double d : dd)
+//		 System.out.print(d + " ");
+//		 System.out.print("\n");
+		
+		// Example 2 from Myers and Miller (1987).
+		 new linear("agtac", "aag");
+		 diff(0, 0, A.length(), B.length());
 	}
 
 	static void diff(int a_start, int b_start, int m, int n) {
@@ -57,15 +54,16 @@ public class linear {
 	}
 
 	static void diff_recurs(int a_start, int b_start, int m, int n, double tb, double te) {
+		// System.out.printf("a_start=%d b_start=%d m=%d n=%d tb=%.2f te=%.2f\n", a_start, b_start, m, n, tb, te);
 		if (n == 0) {
 			if (m > 0) {
-				System.out.println("delete A");
+				System.out.println("delete A=" + A.substring(a_start, a_start + m));
 			}
 		} else if (m == 0) {
-			System.out.println("insert B");
+			System.out.println("insert B=" + B.substring(b_start, b_start + n));
 		} else if (m == 1) {
-			double cost = Math.min(tb, te) + h + gap(n);
-			for (int j = 0; j <= n; j++) {
+			double cost = (Math.min(tb, te) + h) + gap(n);
+			for (int j = 1; j <= n; j++) {
 				double conv2 = gap(j - 1) + w(a_arr[0], b_arr[j - 1]) + gap(n - j);
 				cost = Math.min(cost, conv2);
 			}
@@ -73,21 +71,51 @@ public class linear {
 		} else {
 			int i0 = m / 2;
 			computeCost(a_start, b_start, i0, n, cc, dd, g, true);
-			computeCost(a_start, b_start, m - i0 - 1, n, rr, ss, g, false);
+			computeCost(a_start, b_start, m - i0, n, rr, ss, g, false);
+			
+			// System.out.print("cc = ");
+			// for (double d : cc)
+			// System.out.print(d + " ");
+			// System.out.print("\n");
+			// System.out.print("rr = ");
+			// for (double d : rr)
+			// System.out.print(d + " ");
+			// System.out.print("\n");
+			//
+			// System.out.print("dd = ");
+			// for (double d : dd)
+			// System.out.print(d + " ");
+			// System.out.print("\n");
+			// System.out.print("ss = ");
+			// for (double d : ss)
+			// System.out.print(d + " ");
+			// System.out.print("\n");
+			
+			int j0 = 0;
+			double t1 = cc[0] + rr[n], t2 = dd[0] + ss[n] - g;
+			boolean type1 = (t1 < t2) ? true : false;
+			double mid = Math.min(t1, t2);
 
-			boolean type1 = false;
-			int j0 = (int) Math.min(cc[0] + rr[n], dd[0] + ss[n] - g);
+			// System.out.printf("j=0 type 1 midpoint=%.1f type2 midpoint=%.1f\n", t1, t2);
 			for (int j = 1; j <= n; j++) {
-				double j_curr = Math.min(cc[j] + rr[n - j], dd[j] + ss[n - j] - g);
-				j0 = (int) Math.min(j0, j_curr);
+				t1 = cc[j] + rr[n - j];
+				t2 = dd[j] + ss[n - j] - g;
+				// System.out.printf("j=%d type 1 midpoint=%.1f type2 midpoint=%.1f\n", j, t1, t2);
+				double j_curr = Math.min(t1, t2);
+				if(j_curr < mid) {
+					j0 = j;
+					mid = j_curr;
+					type1 = (t1 < t2) ? true : false;
+				}
 			}
+			// System.out.println("j0=" + j0 + " type1=" + type1);
 
 			if (type1) {
 				diff_recurs(a_start, b_start, i0, j0, tb, g);
 				diff_recurs(a_start + i0 - 1, b_start + j0 - 1, m - i0, n - j0, g, te);
 			} else {
 				diff_recurs(a_start, b_start, i0 - 1, j0, tb, 0);
-				System.out.println("delete " + a_arr[i0 - 1] + a_arr[i0]);
+				System.out.println("delete A[i0 - 1]A[i0]=" + a_arr[i0 - 1] + a_arr[i0]);
 				diff_recurs(a_start + i0, b_start + j0 - 1, m - i0 - 1, n - j0, 0, te);
 			}
 
@@ -124,6 +152,7 @@ public class linear {
 				cc[j] = c;
 			}
 		}
+		dd[0] = cc[0];
 	}
 
 	static double gap(int k) {
