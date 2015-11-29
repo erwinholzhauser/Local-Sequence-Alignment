c7f1f6cde84ea430ab24036c0de871df408cfb58
eholzhauser <erwinholzhauser@gmail.com>
2015-11-29 00:52:57 -0500

    Added local alignment -- globally align the subsequences bracketed by Smith-Waterman indexes

---
+++ src/SmithWaterman.java
@@ -0,0 +1,59 @@
+import java.awt.Point;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SmithWaterman {
+	
+	public static double match = 2, mismatch = -3, alpha = 5, beta = 2, score;
+	
+	public static Point findEndIndices(String a, String b, boolean rev, Point end) {
+		
+		int m = a.length(), n = b.length();
+		
+		double s[] = new double[n+1], f[] = new double[n+1];
+		for(int j = 0; j <= n; j++) {
+			s[j] = 0;
+			f[j] = 0;
+		}
+		
+		double e, up, diagonal;
+		double max_score = 0;
+		Point max_index = new Point();
+		for(int i = 1; i <= m; i++) {
+			s[0] = e = 0;
+			diagonal = 0;
+			for(int j = 1; j <= n; j++) {
+				up = s[j];
+				e = Math.max(s[j-1] - alpha, e - beta);
+				f[j] = Math.max(s[j] - alpha, f[j] - beta);
+				s[j] = Collections.max(Arrays.asList(diagonal + score(a.charAt(i - 1), b.charAt(j - 1)), e, f[j], (double) 0));
+				diagonal = up;
+				
+				// For forward phase, want the left-most local alignment.
+				if(!rev && s[j] > max_score) {
+						max_score = s[j];
+						max_index.setLocation(i, j);
+				}
+				
+				// For the reverse phase, want the right-most local alignment.
+				if(rev && i >= (m - end.x + 1) && j >= (n - end.y + 1) && s[j] >= max_score) {
+						max_score = s[j];
+						max_index.setLocation(i, j);
+				}
+				// System.out.printf(s[j] + " ");
+			}
+			// System.out.printf("\n");
+		}
+		// System.out.println("max_score=" + max_score);
+		
+		if(!rev) {
+			score = max_score;
+		}
+		
+		return max_index;
+	}
+	
+	static double score(char a, char b) {
+		return (a == b) ? match : mismatch;
+	}
+}
--- src/linear.java
+++ src/linear.java
@@ -1,3 +1,4 @@
+import java.awt.Point;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
@@ -23,11 +24,14 @@ public class linear {
 	static LinkedList<Integer> s;
 	static double match = 0, mismatch = 1;
 	static int n = 50;
+	static SmithWaterman sw = new SmithWaterman();
 
 	public static void main(String args[]) {
 
 		Options opt = new Options();
-
+		
+		opt.addOption("a", true, "Path to first sequence file");
+		opt.addOption("b", true, "Path to second sequence file");
 		opt.addOption("h", false, "Print this help message");
 		opt.addOption("g", true, "Gap open penalty");
 		opt.addOption("e", true, "Gap extension penalty");
@@ -38,19 +42,24 @@ public class linear {
 		DefaultParser parser = new DefaultParser();
 		CommandLine cl = null;
 		try {
-			cl = parser.parse(opt, Arrays.copyOfRange(args, 2, args.length));
+			cl = parser.parse(opt, args);
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-
-		if (args.length < 2) {
-			System.out.println("Give two files with sequences to align");
-			return;
+		
+		String usage = "java -jar linear.jar -a <path/to/first/sequence> -b <path/to/second/sequence> [parameters]";
+		
+		if (cl.hasOption("h")) {
+			HelpFormatter f = new HelpFormatter();
+			f.printHelp(usage, opt);
+		}
+		
+		if(!cl.hasOption("a") || !cl.hasOption("b")) {
+			System.out.println(usage);
 		} else {
-
 			Scanner fin = null;
 			try {
-				fin = new Scanner(new File(args[0]));
+				fin = new Scanner(new File(cl.getOptionValue("a")));
 			} catch (FileNotFoundException e) {
 				System.out.println("Invalid path to first sequence file");
 				e.printStackTrace();
@@ -58,9 +67,14 @@ public class linear {
 			while (fin.hasNext()) {
 				A += fin.next();
 			}
+			
+			if(A.length() == 0) {
+				System.out.println("Your first sequence is empty.");
+				return;
+			}
 
 			try {
-				fin = new Scanner(new File(args[1]));
+				fin = new Scanner(new File(cl.getOptionValue("b")));
 			} catch (FileNotFoundException e) {
 				System.out.println("Invalid path to second sequence file");
 				e.printStackTrace();
@@ -68,61 +82,90 @@ public class linear {
 			while (fin.hasNext()) {
 				B += fin.next();
 			}
-		}
-
-		if (cl.hasOption("h")) {
-			HelpFormatter f = new HelpFormatter();
-			f.printHelp("java -jar linear.jar ...", opt);
-		}
-		if (cl.hasOption("g")) {
-			try {
-				g = Double.parseDouble(cl.getOptionValue("g"));
-			} catch (NumberFormatException e) {
-				System.out.println("Gap open penalty must be a number");
+			
+			if(B.length() == 0) {
+				System.out.println("Your second sequence is empty.");
+				return;
 			}
-		}
-		if (cl.hasOption("e")) {
-			try {
-				h = Double.parseDouble(cl.getOptionValue("e"));
-			} catch (NumberFormatException e) {
-				System.out.println("Gap extension penalty must be a number");
+			
+			if (cl.hasOption("g")) {
+				try {
+					sw.alpha = Double.parseDouble(cl.getOptionValue("g"));
+				} catch (NumberFormatException e) {
+					System.out.println("Gap open penalty must be a number");
+				}
 			}
-		}
-		if (cl.hasOption("m")) {
-			try {
-				match = Double.parseDouble(cl.getOptionValue("m"));
-			} catch (NumberFormatException e) {
-				System.out.println("Match score must be a number");
+			if (cl.hasOption("e")) {
+				try {
+					sw.beta = Double.parseDouble(cl.getOptionValue("e"));
+				} catch (NumberFormatException e) {
+					System.out.println("Gap extension penalty must be a number");
+				}
 			}
-		}
-		if (cl.hasOption("i")) {
-			try {
-				mismatch = Double.parseDouble(cl.getOptionValue("i"));
-			} catch (NumberFormatException e) {
-				System.out.println("Mismatch score must be a number");
+			if (cl.hasOption("m")) {
+				try {
+					sw.match = Double.parseDouble(cl.getOptionValue("m"));
+				} catch (NumberFormatException e) {
+					System.out.println("Match score must be a number");
+				}
 			}
-		}
-		if (cl.hasOption("n")) {
-			try {
-				linear.n = Integer.parseInt(cl.getOptionValue("n"));
-			} catch (NumberFormatException e) {
-				System.out
-						.println("Max characters per line must be an integer");
+			if (cl.hasOption("i")) {
+				try {
+					sw.mismatch = Double.parseDouble(cl.getOptionValue("i"));
+				} catch (NumberFormatException e) {
+					System.out.println("Mismatch score must be a number");
+				}
 			}
+			if (cl.hasOption("n")) {
+				try {
+					linear.n = Integer.parseInt(cl.getOptionValue("n"));
+				} catch (NumberFormatException e) {
+					System.out.println("Max characters per line must be an integer");
+				}
+			}
+			
+			/*
+			 * Local Alignment
+			 */
+			new linear(linear.A, linear.B);
+			int a_len = a_arr.length, b_len = b_arr.length;
+			
+			/*
+			 * Find start and end indices of the leftmost local alignment with a linear-space,
+			 * score-only version of Smith-Waterman.
+			 */
+			Point end_indices = sw.findEndIndices(A, B, false, new Point());
+			// System.out.println("end_indices=" + end_indices);
+			Point start_indices = sw.findEndIndices(new String(rev_a), new String(rev_b), true, end_indices);
+			// System.out.println("start_indices=" + start_indices);
+			start_indices.x = a_len - start_indices.x + 1;
+			start_indices.y = b_len - start_indices.y + 1;
+			// System.out.println("start_indices=" + start_indices);
+			
+			/*
+			 * Use Myers and Miller recursive, divide-and-conquer strategy to align the subsequences
+			 * bracketed by the local alignment using Gotoh's linear-space conversion cost.
+			 */
+			diff(start_indices.x - 1, start_indices.y - 1, end_indices.x - start_indices.x + 1, end_indices.y - start_indices.y + 1);
+			display(n, start_indices.x, end_indices.x, start_indices.y, end_indices.y);
+			
+			
+			/*
+			 * Global Alignment.
+			 */
+			// int a_len = a_arr.length, b_len = b_arr.length;
+			// diff(0, 0, a_len, b_len);
+			// display(n, 1, a_len, 1, b_len);
 		}
-		
-		new linear(linear.A, linear.B);
-		diff(0, 0, A.length(), B.length());
-		display(n);
 	}
 
 	static void init(String A, String B) {
 		linear.A = A.toLowerCase();
 		linear.B = B.toLowerCase();
 
-		int m = A.length(), n = B.length();
 		a_arr = linear.A.toCharArray();
 		b_arr = linear.B.toCharArray();
+		int m = a_arr.length, n = b_arr.length;
 
 		rev_a = new char[m];
 		rev_b = new char[n];
@@ -135,18 +178,66 @@ public class linear {
 			rev_b[n - i - 1] = b_arr[i];
 		}
 
-		cc = new double[m + 1];
-		dd = new double[m + 1];
+		/*
+		 * As defined in Myers and Miller (1988), but maybe erroneous.
+		 * Reasoning: The vector is supposed to replace an |A|x|B| substitution
+		 * matrix, by only maintaining a row of interest. Rows are of length |B|
+		 * = n, i.e. aligning some A[1...i] to B.
+		 */
+		// cc = new double[m + 1];
+		// dd = new double[m + 1];
+		
+		cc = new double[n + 1];
+		dd = new double[n + 1];
 		rr = new double[n + 1];
 		ss = new double[n + 1];
 		s = new LinkedList<Integer>();
 	}
 
-	static void display(int n) {
+	static void display(int n, int a_start, int a_end, int b_start, int b_end) {
+		
 		int a_ctr = 0, b_ctr = 0;
 		String a = "";
 		String b = "";
 		String r = "";
+		
+		if(b_start > a_start) {
+			int spaces = b_start - a_start;
+			for(int i = 0; i < b_start - 1; i++) {
+				b += b_arr[b_ctr];
+				if(i >= spaces) {
+					a += a_arr[a_ctr];
+					a_ctr++;
+				} else {
+					a += " ";
+				}
+				r += " ";
+				b_ctr++;
+			}
+		} else if(a_start > b_start) {
+			int spaces = a_start - b_start;
+			for(int i = 0; i < a_start - 1; i++) {
+				a += a_arr[a_ctr];
+				if(i >= spaces) {
+					b += b_arr[b_ctr];
+					b_ctr++;
+				} else {
+					b += " ";
+				}
+				r += " ";
+				a_ctr++;
+			}
+		} else {
+			for(int i = 0; i < a_start - 1; i++) {
+				a += a_arr[a_ctr];
+				b += b_arr[b_ctr];
+				r += " ";
+				a_ctr++;
+				b_ctr++;
+			}
+		}
+		
+		int length = 0, aligned = 0, gaps = 0;
 		for (int i : s) {
 			if (i < 0) {
 				for (int j = 0; j < Math.abs(i); j++) {
@@ -154,21 +245,49 @@ public class linear {
 					b += " ";
 					r += "-";
 					a_ctr++;
+					length++;
 				}
+				gaps++;
 			} else if (i > 0) {
 				for (int j = 0; j < i; j++) {
 					a += " ";
 					b += b_arr[b_ctr];
 					r += "-";
 					b_ctr++;
+					length++;
 				}
+				gaps++;
 			} else {
 				a += a_arr[a_ctr];
 				b += b_arr[b_ctr];
 				r += ((a_arr[a_ctr] == b_arr[b_ctr]) ? "!" : "|");
 				a_ctr++;
 				b_ctr++;
+				
+				if(a_arr[a_ctr - 1] == b_arr[b_ctr - 1]) {
+					aligned++;
+				}
+				length++;
 			}
+		}
+		
+		int a_len = a_arr.length, b_len = b_arr.length;
+		for(int i = 0; i < Math.max(a_len - a_end, b_len - b_end); i++) {
+			if(a_ctr < a_len) {
+				a += a_arr[a_ctr];
+				a_ctr++;
+			} else {
+				a += " ";
+			}
+			
+			if(b_ctr < b_len) {
+				b += b_arr[b_ctr];
+				b_ctr++;
+			} else {
+				b += " ";
+			}
+			
+			r += " ";
 		}
 
 		String d = "";
@@ -181,25 +300,35 @@ public class linear {
 				d += " ";
 			}
 		}
+		
+		System.out.println("#=======================================");
+		System.out.println("#");
+		System.out.println("# Gap penalty: " + sw.alpha);
+		System.out.println("# Extension penalty: " + sw.beta);
+		System.out.println("# Match score: " + sw.match);
+		System.out.println("# Mismatch score: " + sw.mismatch);
+		System.out.println("#");
+		System.out.println("# Length: " + length);
+		System.out.printf("# Identity: %d/%d (%.1f%%)\n", aligned, length, ((double) aligned/length)*100);
+		System.out.printf("# Gaps: %d/%d (%.1f%%)\n", gaps, length, ((double) gaps/length)*100);
+		System.out.println("# Score: " + sw.score);
+		System.out.println("#");
+		System.out.println("#=======================================\n\n");
 
 		int i = r.length(), ctr = 0;
 		while (i >= n) {
 			String c = new Integer(ctr).toString();
 			int c_len = c.length();
 			String sp = new String(new char[c_len]).replace("\0", " ");
-			System.out.println(c
-					+ d.substring(r.length() - i, r.length() - i + n));
-			System.out.println(sp
-					+ b.substring(r.length() - i, r.length() - i + n));
-			System.out.println(sp
-					+ r.substring(r.length() - i, r.length() - i + n));
-			System.out.println(sp
-					+ a.substring(r.length() - i, r.length() - i + n));
+			System.out.println(c + d.substring(r.length() - i, r.length() - i + n));
+			System.out.println(sp + b.substring(r.length() - i, r.length() - i + n));
+			System.out.println(sp + r.substring(r.length() - i, r.length() - i + n));
+			System.out.println(sp + a.substring(r.length() - i, r.length() - i + n));
 			System.out.println();
 			i -= n;
 			ctr += n;
 		}
-		if(d.length() - (r.length() - i) > 0) {			
+		if (d.length() - (r.length() - i) > 0) {
 			String c = new Integer(ctr).toString();
 			int c_len = c.length();
 			String sp = new String(new char[c_len]).replace("\0", " ");
@@ -217,15 +346,14 @@ public class linear {
 
 	static void diff_recurs(int a_start, int b_start, int m, int n, double tb,
 			double te) {
+		// System.out.printf("a_start=%d b_start=%d m=%d n=%d\n", a_start, b_start, m, n);
 		if (n == 0) {
 			if (m > 0) {
-				// System.out.println("delete A=" + A.substring(a_start, a_start
-				// + m));
+				// System.out.println("delete A=" + A.substring(a_start, a_start + m));
 				s.addLast(-m);
 			}
 		} else if (m == 0) {
-			// System.out.println("insert B=" + B.substring(b_start, b_start +
-			// n));
+			// System.out.println("insert B=" + B.substring(b_start, b_start + n));
 			s.addLast(n);
 		} else if (m == 1) {
 			int ctr = 2;
@@ -283,11 +411,9 @@ public class linear {
 				diff_recurs(a_start + i0, b_start + j0, m - i0, n - j0, g, te);
 			} else {
 				diff_recurs(a_start, b_start, i0 - 1, j0, tb, 0);
-				// System.out.println("delete A[i0 - 1]A[i0]=" + a_arr[i0 - 1] +
-				// a_arr[i0]);
+				// System.out.println("delete A[i0 - 1]A[i0]=" + a_arr[i0 - 1] + a_arr[i0]);
 				s.addLast(-2);
-				diff_recurs(a_start + i0 + 1, b_start + j0, m - i0 - 1, n - j0,
-						0, te);
+				diff_recurs(a_start + i0 + 1, b_start + j0, m - i0 - 1, n - j0, 0, te);
 			}
 
 		}
@@ -317,7 +443,7 @@ public class linear {
 				if (forward) {
 					c = Math.min(dd[j], Math.min(e, s + w(a_arr[a_s + i - 1], b_arr[b_s + j - 1])));
 				} else {
-					c = Math.min(dd[j], Math.min(e, s + w(rev_a[a_s + i - 1], rev_b[b_s+ j - 1])));
+					c = Math.min(dd[j], Math.min(e, s + w(rev_a[a_s + i - 1], rev_b[b_s + j - 1])));
 				}
 
 				s = cc[j];
